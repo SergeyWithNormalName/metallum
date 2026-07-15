e1fb4e5769e3a1c06e540a3b6c6d59b5b5ad80be
Sergey228i666i69 <cgenerozov@gmail.com>
2026-07-15 16:32:11 +0700

    feat: update block renderer mixin for special emissive sources and update jar

--- src/main/java/com/metallum/mixin/sodium/BlockRendererHdrMixin.java
+++ src/main/java/com/metallum/mixin/sodium/BlockRendererHdrMixin.java
@@ -1,12 +1,17 @@
 package com.metallum.mixin.sodium;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.metallum.client.hdr.SodiumHdrSemantic;
+import net.caffeinemc.mods.sodium.client.model.light.LightMode;
 import net.caffeinemc.mods.sodium.client.render.chunk.terrain.material.Material;
 import net.caffeinemc.mods.sodium.client.render.chunk.vertex.format.ChunkVertexEncoder;
 import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
 import net.caffeinemc.mods.sodium.client.render.model.MutableQuadViewImpl;
+import net.caffeinemc.mods.sodium.client.render.model.SodiumShadeMode;
 import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,6 +28,9 @@ abstract class BlockRendererHdrMixin {
     private ChunkVertexEncoder.Vertex[] vertices;
 
     @Unique
+    private BlockState metallum$blockState;
+
+    @Unique
     private int metallum$blockLightEmission;
 
     @Inject(method = "renderModel", at = @At("HEAD"), remap = false)
@@ -33,7 +41,28 @@ abstract class BlockRendererHdrMixin {
             final BlockPos origin,
             final CallbackInfo ci
     ) {
+        this.metallum$blockState = state;
         this.metallum$blockLightEmission = state.getLightEmission();
+    }
+
+    @WrapOperation(
+            method = "processQuad(Lnet/caffeinemc/mods/sodium/client/render/model/MutableQuadViewImpl;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderer;shadeQuad(Lnet/caffeinemc/mods/sodium/client/render/model/MutableQuadViewImpl;Lnet/caffeinemc/mods/sodium/client/model/light/LightMode;ZLnet/caffeinemc/mods/sodium/client/render/model/SodiumShadeMode;)V"
+            ),
+            remap = false
+    )
+    private void metallum$overrideEmissive(
+            final BlockRenderer renderer,
+            final MutableQuadViewImpl quad,
+            final LightMode lightMode,
+            final boolean emissive,
+            final SodiumShadeMode shadeMode,
+            final Operation<Void> original
+    ) {
+        boolean overriddenEmissive = emissive || metallum$isSpecialEmissiveSource();
+        original.call(renderer, quad, lightMode, overriddenEmissive, shadeMode);
     }
 
     @Inject(
@@ -54,5 +83,18 @@ abstract class BlockRendererHdrMixin {
         boolean exact = quad.emissive();
         int emission = exact ? 15 : this.metallum$blockLightEmission;
         SodiumHdrSemantic.tagQuad(this.vertices, emission, exact);
+    }
+
+    @Unique
+    private boolean metallum$isSpecialEmissiveSource() {
+        if (this.metallum$blockState == null) {
+            return false;
+        }
+        return this.metallum$blockState.is(Blocks.SOUL_FIRE)
+                || this.metallum$blockState.is(Blocks.SOUL_TORCH)
+                || this.metallum$blockState.is(Blocks.SOUL_WALL_TORCH)
+                || this.metallum$blockState.is(Blocks.SOUL_LANTERN)
+                || this.metallum$blockState.is(Blocks.SOUL_CAMPFIRE)
+                || this.metallum$blockState.is(Blocks.NETHER_PORTAL);
     }
 }
